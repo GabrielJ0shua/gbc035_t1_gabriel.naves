97ec2c913ed6544581e4815ee2c096ad67123c24
Gabriel Joshua <gjoshuacalixto@outlook.com>
2023-03-12 15:35:03 -0300

    exe1 feito e regularizacao do git pelo terminal

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,10 @@
+package ex01;
+
+public class HelloWorld{
+
+    public static void main(String[] arg){
+        System.out.println("Hello World");
+        System.out.println("Gabriel Joshua Calixto Naves dos Santos");
+        System.out.println("11911BCC052");
+    }
+}
